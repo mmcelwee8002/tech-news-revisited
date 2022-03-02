750caebd309886796d9a2728f38cc968f25112e0
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-03-02 13:09:24 -0600

    lesson 2

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
-
+const bcrypt = require('bcrypt');
 // create our User model
 class User extends Model { }
 
@@ -45,10 +45,24 @@ User.init(
             }
         }
     },
-    
+
+    {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+                newUserData.password = await bcrypt.hash(newUserData.password, 10);
+                return newUserData;
+                
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData;
+            }
+        },
     
         // TABLE CONFIGURATION OPTIONS GO HERE (https://sequelize.org/v5/manual/models-definition.html#configuration))
- {
+ 
            // pass in our imported sequelize connection (the direct connection to our database)
        
         sequelize,
