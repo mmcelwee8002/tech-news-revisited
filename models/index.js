fb47b75e56f322a6b906631eccbb84240dc0f5d9
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-03-02 15:10:12 -0600

    fuck

--- models/index.js
+++ models/index.js
@@ -1,13 +1,14 @@
+// import all models
+const Post = require('./Post');
 const User = require('./User');
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
 
+Post.belongsTo(User, {
+    foreignKey: 'user_id'
+});
 
-
-
-
-
-
-
-
-
-module.exports = { User };
+module.exports = { User, Post };
