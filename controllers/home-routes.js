62fba4451b1085d4b1b89ed9ebd5301b03b0fee7
mmcelwee8002 <mmcelwee8002@gmail.com>
2022-03-06 15:49:29 -0600

    lesson 14-3

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
+const { Post, User, Comment, Vote } = require('../models');
 
-
+// get all posts for homepage
 router.get('/', (req, res) => {
     console.log('======================');
     Post.findAll({
@@ -29,9 +29,12 @@ router.get('/', (req, res) => {
         ]
     })
         .then(dbPostData => {
-            // pass a single post object into the homepage template
             const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('homepage', { posts });
+
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
@@ -39,17 +42,7 @@ router.get('/', (req, res) => {
         });
 });
 
-router.get('/login', (req, res) => {
-    if (req.session.loggedIn) {
-        res.redirect('/');
-        return;
-    }
-
-    res.render('login');
-});
-
-
-
+// get single post
 router.get('/post/:id', (req, res) => {
     Post.findOne({
         where: {
@@ -83,16 +76,26 @@ router.get('/post/:id', (req, res) => {
                 return;
             }
 
-            // serialize the data
             const post = dbPostData.get({ plain: true });
 
-            // pass data to template
-            res.render('single-post', { post });
+            res.render('single-post', {
+                post,
+                loggedIn: req.session.loggedIn
+            });
         })
         .catch(err => {
             console.log(err);
             res.status(500).json(err);
         });
+});
+
+router.get('/login', (req, res) => {
+    if (req.session.loggedIn) {
+        res.redirect('/');
+        return;
+    }
+
+    res.render('login');
 });
 
 module.exports = router;
